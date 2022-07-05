0f54dd60a99eec48e06458f982d367e8e75efc2b
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2022-07-05 17:26:40 -0300

    redux working

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,36 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+//redux
+import {fetchAllUsers} from '../store/slices/users'
+import {useDispatch} from 'react-redux'
 
 export default function UserList() {
+
+  const dispatch = useDispatch();  
+
   const users = [
-    {
-      "id": 7,
-      "email": "michael.lawson@reqres.in",
-      "first_name": "Michael",
-      "last_name": "Lawson",
-      "avatar": "https://reqres.in/img/faces/7-image.jpg"
-    },
-    {
-      "id": 8,
-      "email": "lindsay.ferguson@reqres.in",
-      "first_name": "Lindsay",
-      "last_name": "Ferguson",
-      "avatar": "https://reqres.in/img/faces/8-image.jpg"
-    },
-    {
-      "id": 9,
-      "email": "tobias.funke@reqres.in",
-      "first_name": "Tobias",
-      "last_name": "Funke",
-      "avatar": "https://reqres.in/img/faces/9-image.jpg"
-    },
-    {
-      "id": 10,
-      "email": "byron.fields@reqres.in",
-      "first_name": "Byron",
-      "last_name": "Fields",
-      "avatar": "https://reqres.in/img/faces/10-image.jpg"
-    },
   ]
+  useEffect(()=>{
+    dispatch(fetchAllUsers())
+  },[])
+
   return (
     <div className='container'>
       <div className='row'>
